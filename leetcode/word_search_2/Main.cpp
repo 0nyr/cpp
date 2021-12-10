393b7dcf999113ade88306c7fa9c0d5d7d0763d4
onyr <rascoussier.florian@gmail.com>
2021-12-10 09:47:52 +0100

    Final successful code for word search II.

--- leetcode/word_search_2/Main.cpp
+++ leetcode/word_search_2/Main.cpp
@@ -1,6 +1,18 @@
+/* Word search II
+@Author: Onyr (Florian Rascoussier)
+
+Ideas:
+    + eliminate words with characters that are not in the board
+    + DFS: use DFS to search the board efficiently
+
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <algorithm>
 
 class Position {
 public:
@@ -160,6 +172,46 @@ private:
         return visited;
     }
 
+    bool is_word_valid(
+        std::string word, 
+        std::vector<std::vector<char>> const& board,
+        std::vector<char> const& all_characters_in_board
+    ) {
+        bool word_is_valid = true;
+        for(char c : word) {
+            if(std::find(
+                all_characters_in_board.begin(), 
+                all_characters_in_board.end(), 
+                c
+            ) == all_characters_in_board.end()) {
+                // word contains a character not present in the board
+                // eliminate it
+                word_is_valid = false;
+                break;
+            }
+        }
+        return word_is_valid;
+    }
+
+    std::vector<char> create_vector_of_all_different_characters_in_board(
+        std::vector<std::vector<char>> const& board
+    ) {
+        std::vector<char> all_characters;
+        for(int i = 0; i < board.size(); ++i) {
+            for(int j = 0; j < board[i].size(); ++j) {
+                // add character to vector if not already present
+                if(
+                std::find(
+                    all_characters.begin(), all_characters.end(), 
+                    board[i][j]
+                ) == all_characters.end()) {
+                    all_characters.push_back(board[i][j]);
+                }
+            }
+        }
+        return all_characters;
+    }
+
 public:
     std::vector<std::string> findWords(
         std::vector<std::vector<char>>& board, 
@@ -167,11 +219,24 @@ public:
     ) { 
         std::vector<std::string> present_words;
 
+        // build vector of all characters in the board
+        std::vector<char> all_characters_in_board = 
+            create_vector_of_all_different_characters_in_board(board);
+        
         // search each provided word in the board
         for (std::string& word : words) {
+            // eliminate words with characters not present in the board
+            bool word_is_valid = is_word_valid(
+                word, board, all_characters_in_board
+            );
+            if(!word_is_valid) {
+                // word is not valid, skip it
+                continue;
+            }
+
+            // check if word is present in the board with DFS
             std::vector<std::vector<bool>> visited;
             Position position_last_letter_found = Position(-1, -1);
-
             if (dfs(board, visited, word, 0, position_last_letter_found)) 
             {
                 present_words.push_back(word);
@@ -189,6 +254,9 @@ public:
 int main() {
     const char * url = "https://leetcode.com/problems/word-search-ii/";
     std::cout << "Leetcode Word Search II problem: " << url << std::endl;
+
+    // start timer
+    auto start_time = std::chrono::high_resolution_clock::now();
     
     // test 1
     std::vector<std::vector<char>> grid_1 = {
@@ -254,6 +322,173 @@ int main() {
         std::cout << "   + " << word << std::endl;
     }
     std::cout << std::endl;
+
+    
+    // test 5
+    std::vector<std::vector<char>> grid_5 = {
+        {'b','a','b','a','b','a','b','a','b','a'},
+        {'a','b','a','b','a','b','a','b','a','b'},
+        {'b','a','b','a','b','a','b','a','b','a'},
+        {'a','b','a','b','a','b','a','b','a','b'},
+        {'b','a','b','a','b','a','b','a','b','a'},
+        {'a','b','a','b','a','b','a','b','a','b'},
+        {'b','a','b','a','b','a','b','a','b','a'},
+        {'a','b','a','b','a','b','a','b','a','b'},
+        {'b','a','b','a','b','a','b','a','b','a'},
+        {'a','b','a','b','a','b','a','b','a','b'},
+    };
+    std::vector<std::string> words_5 = {
+        "ababababaa","ababababab","ababababac","ababababad","ababababae",
+        "ababababaf","ababababag","ababababah","ababababai","ababababaj",
+        "ababababak","ababababal","ababababam","ababababan","ababababao",
+        "ababababap","ababababaq","ababababar","ababababas","ababababat",
+        "ababababau","ababababav","ababababaw","ababababax","ababababay",
+        "ababababaz","ababababba","ababababbb","ababababbc","ababababbd",
+        "ababababbe","ababababbf","ababababbg","ababababbh","ababababbi",
+        "ababababbj","ababababbk","ababababbl","ababababbm","ababababbn",
+        "ababababbo","ababababbp","ababababbq","ababababbr","ababababbs",
+        "ababababbt","ababababbu","ababababbv","ababababbw","ababababbx",
+        "ababababby","ababababbz","ababababca","ababababcb","ababababcc",
+        "ababababcd","ababababce","ababababcf","ababababcg","ababababch",
+        "ababababci","ababababcj","ababababck","ababababcl","ababababcm",
+        "ababababcn","ababababco","ababababcp","ababababcq","ababababcr",
+        "ababababcs","ababababct","ababababcu","ababababcv","ababababcw",
+        "ababababcx","ababababcy","ababababcz","ababababda","ababababdb",
+        "ababababdc","ababababdd","ababababde","ababababdf","ababababdg",
+        "ababababdh","ababababdi","ababababdj","ababababdk","ababababdl",
+        "ababababdm","ababababdn","ababababdo","ababababdp","ababababdq",
+        "ababababdr","ababababds","ababababdt","ababababdu","ababababdv",
+        "ababababdw","ababababdx","ababababdy","ababababdz","ababababea",
+        "ababababeb","ababababec","ababababed","ababababee","ababababef",
+        "ababababeg","ababababeh","ababababei","ababababej","ababababek",
+        "ababababel","ababababem","ababababen","ababababeo","ababababep",
+        "ababababeq","ababababer","ababababes","ababababet","ababababeu",
+        "ababababev","ababababew","ababababex","ababababey","ababababez",
+        "ababababfa","ababababfb","ababababfc","ababababfd","ababababfe",
+        "ababababff","ababababfg","ababababfh","ababababfi","ababababfj",
+        "ababababfk","ababababfl","ababababfm","ababababfn","ababababfo",
+        "ababababfp","ababababfq","ababababfr","ababababfs","ababababft",
+        "ababababfu","ababababfv","ababababfw","ababababfx","ababababfy",
+        "ababababfz","ababababga","ababababgb","ababababgc","ababababgd",
+        "ababababge","ababababgf","ababababgg","ababababgh","ababababgi",
+        "ababababgj","ababababgk","ababababgl","ababababgm","ababababgn",
+        "ababababgo","ababababgp","ababababgq","ababababgr","ababababgs",
+        "ababababgt","ababababgu","ababababgv","ababababgw","ababababgx",
+        "ababababgy","ababababgz","ababababha","ababababhb","ababababhc",
+        "ababababhd","ababababhe","ababababhf","ababababhg","ababababhh",
+        "ababababhi","ababababhj","ababababhk","ababababhl","ababababhm",
+        "ababababhn","ababababho","ababababhp","ababababhq","ababababhr",
+        "ababababhs","ababababht","ababababhu","ababababhv","ababababhw",
+        "ababababhx","ababababhy","ababababhz","ababababia","ababababib",
+        "ababababic","ababababid","ababababie","ababababif","ababababig",
+        "ababababih","ababababii","ababababij","ababababik","ababababil",
+        "ababababim","ababababin","ababababio","ababababip","ababababiq",
+        "ababababir","ababababis","ababababit","ababababiu","ababababiv",
+        "ababababiw","ababababix","ababababiy","ababababiz","ababababja",
+        "ababababjb","ababababjc","ababababjd","ababababje","ababababjf",
+        "ababababjg","ababababjh","ababababji","ababababjj","ababababjk",
+        "ababababjl","ababababjm","ababababjn","ababababjo","ababababjp",
+        "ababababjq","ababababjr","ababababjs","ababababjt","ababababju",
+        "ababababjv","ababababjw","ababababjx","ababababjy","ababababjz",
+        "ababababka","ababababkb","ababababkc","ababababkd","ababababke",
+        "ababababkf","ababababkg","ababababkh","ababababki","ababababkj",
+        "ababababkk","ababababkl","ababababkm","ababababkn","ababababko",
+        "ababababkp","ababababkq","ababababkr","ababababks","ababababkt",
+        "ababababku","ababababkv","ababababkw","ababababkx","ababababky",
+        "ababababkz","ababababla","abababablb","abababablc","ababababld",
+        "abababable","abababablf","abababablg","abababablh","ababababli",
+        "abababablj","abababablk","ababababll","abababablm","ababababln",
+        "abababablo","abababablp","abababablq","abababablr","ababababls",
+        "abababablt","abababablu","abababablv","abababablw","abababablx",
+        "abababably","abababablz","ababababma","ababababmb","ababababmc",
+        "ababababmd","ababababme","ababababmf","ababababmg","ababababmh",
+        "ababababmi","ababababmj","ababababmk","ababababml","ababababmm",
+        "ababababmn","ababababmo","ababababmp","ababababmq","ababababmr",
+        "ababababms","ababababmt","ababababmu","ababababmv","ababababmw",
+        "ababababmx","ababababmy","ababababmz","ababababna","ababababnb",
+        "ababababnc","ababababnd","ababababne","ababababnf","ababababng",
+        "ababababnh","ababababni","ababababnj","ababababnk","ababababnl",
+        "ababababnm","ababababnn","ababababno","ababababnp","ababababnq",
+        "ababababnr","ababababns","ababababnt","ababababnu","ababababnv",
+        "ababababnw","ababababnx","ababababny","ababababnz","ababababoa",
+        "ababababob","ababababoc","ababababod","ababababoe","ababababof",
+        "ababababog","ababababoh","ababababoi","ababababoj","ababababok",
+        "ababababol","ababababom","ababababon","ababababoo","ababababop",
+        "ababababoq","ababababor","ababababos","ababababot","ababababou",
+        "ababababov","ababababow","ababababox","ababababoy","ababababoz",
+        "ababababpa","ababababpb","ababababpc","ababababpd","ababababpe",
+        "ababababpf","ababababpg","ababababph","ababababpi","ababababpj",
+        "ababababpk","ababababpl","ababababpm","ababababpn","ababababpo",
+        "ababababpp","ababababpq","ababababpr","ababababps","ababababpt",
+        "ababababpu","ababababpv","ababababpw","ababababpx","ababababpy",
+        "ababababpz","ababababqa","ababababqb","ababababqc","ababababqd",
+        "ababababqe","ababababqf","ababababqg","ababababqh","ababababqi",
+        "ababababqj","ababababqk","ababababql","ababababqm","ababababqn",
+        "ababababqo","ababababqp","ababababqq","ababababqr","ababababqs",
+        "ababababqt","ababababqu","ababababqv","ababababqw","ababababqx",
+        "ababababqy","ababababqz","ababababra","ababababrb","ababababrc",
+        "ababababrd","ababababre","ababababrf","ababababrg","ababababrh",
+        "ababababri","ababababrj","ababababrk","ababababrl","ababababrm",
+        "ababababrn","ababababro","ababababrp","ababababrq","ababababrr",
+        "ababababrs","ababababrt","ababababru","ababababrv","ababababrw",
+        "ababababrx","ababababry","ababababrz","ababababsa","ababababsb",
+        "ababababsc","ababababsd","ababababse","ababababsf","ababababsg",
+        "ababababsh","ababababsi","ababababsj","ababababsk","ababababsl",
+        "ababababsm","ababababsn","ababababso","ababababsp","ababababsq",
+        "ababababsr","ababababss","ababababst","ababababsu","ababababsv",
+        "ababababsw","ababababsx","ababababsy","ababababsz","ababababta",
+        "ababababtb","ababababtc","ababababtd","ababababte","ababababtf",
+        "ababababtg","ababababth","ababababti","ababababtj","ababababtk",
+        "ababababtl","ababababtm","ababababtn","ababababto","ababababtp",
+        "ababababtq","ababababtr","ababababts","ababababtt","ababababtu",
+        "ababababtv","ababababtw","ababababtx","ababababty","ababababtz",
+        "ababababua","ababababub","ababababuc","ababababud","ababababue",
+        "ababababuf","ababababug","ababababuh","ababababui","ababababuj",
+        "ababababuk","ababababul","ababababum","ababababun","ababababuo",
+        "ababababup","ababababuq","ababababur","ababababus","ababababut",
+        "ababababuu","ababababuv","ababababuw","ababababux","ababababuy",
+        "ababababuz","ababababva","ababababvb","ababababvc","ababababvd",
+        "ababababve","ababababvf","ababababvg","ababababvh","ababababvi",
+        "ababababvj","ababababvk","ababababvl","ababababvm","ababababvn",
+        "ababababvo","ababababvp","ababababvq","ababababvr","ababababvs",
+        "ababababvt","ababababvu","ababababvv","ababababvw","ababababvx",
+        "ababababvy","ababababvz","ababababwa","ababababwb","ababababwc",
+        "ababababwd","ababababwe","ababababwf","ababababwg","ababababwh",
+        "ababababwi","ababababwj","ababababwk","ababababwl","ababababwm",
+        "ababababwn","ababababwo","ababababwp","ababababwq","ababababwr",
+        "ababababws","ababababwt","ababababwu","ababababwv","ababababww",
+        "ababababwx","ababababwy","ababababwz","ababababxa","ababababxb",
+        "ababababxc","ababababxd","ababababxe","ababababxf","ababababxg",
+        "ababababxh","ababababxi","ababababxj","ababababxk","ababababxl",
+        "ababababxm","ababababxn","ababababxo","ababababxp","ababababxq",
+        "ababababxr","ababababxs","ababababxt","ababababxu","ababababxv",
+        "ababababxw","ababababxx","ababababxy","ababababxz","ababababya",
+        "ababababyb","ababababyc","ababababyd","ababababye","ababababyf",
+        "ababababyg","ababababyh","ababababyi","ababababyj","ababababyk",
+        "ababababyl","ababababym","ababababyn","ababababyo","ababababyp",
+        "ababababyq","ababababyr","ababababys","ababababyt","ababababyu",
+        "ababababyv","ababababyw","ababababyx","ababababyy","ababababyz",
+        "ababababza","ababababzb","ababababzc","ababababzd","ababababze",
+        "ababababzf","ababababzg","ababababzh","ababababzi","ababababzj",
+        "ababababzk","ababababzl","ababababzm","ababababzn","ababababzo",
+        "ababababzp","ababababzq","ababababzr","ababababzs","ababababzt",
+        "ababababzu","ababababzv","ababababzw","ababababzx","ababababzy",
+        "ababababzz","ababacabaa","ababacabab","ababacabac","ababacabad",
+    };
+
+    // test 4 solution
+    std::vector<std::string> solution_5 = Solution().findWords(grid_5, words_5);
+    std::cout << "Solution 5: " << std::endl;
+    for (auto word : solution_5) {
+        std::cout << "   + " << word << std::endl;
+    }
+    std::cout << std::endl;
+
+
+    // end timer
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_time = end_time - start_time;
+    std::cout << "Elapsed time: " << elapsed_time.count() << " s" << std::endl;
 
     return 0;
 }
